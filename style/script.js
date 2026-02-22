498a4370967a6266450ef519261738f179e96bce
mehedy215781 <mehedy215781@gmail.com>
2026-02-22 17:26:24 +0600

    button-section-edit-inside-card

--- style/script.js
+++ style/script.js
@@ -5,22 +5,29 @@
 let total = document.getElementById('Total');
 let interviewCount = document.getElementById('Interview-count');
 let rejectedCount = document.getElementById('Rejected-count');
-// console.log(total);
+console.log(interviewCount);
+
 const allCardSections = document.getElementById('all-cards ');
 console.log(allCardSections.children.length);
+
+
+const mainContainer = document.querySelector('main');
+const filterSection = document.getElementById('filter-section');
+
+// console.log(mainContainer);
+
+function calculateTotall (){
+    total.innerText = allCardSections.children.length;
+    interviewCount.innerText = InterviewList.length;
+     rejectedCount.innerText = rejectedList.length;
+}
+calculateTotall()
 
 // buttonColrchange//
 const allBtn = document.getElementById('btn-all');
 const interviewBtn = document.getElementById('btn-Interview');
 const rejectedBtn = document.getElementById('btn-Rejected');
 
-
-function calculateTotall (){
-    total.innerText = allCardSections.children.length;
-    interviewCount.innerText =InterviewList.length;
-     rejectedCount.innerText =rejectedList.length;
-}
-calculateTotall()
 
 function toggleStyle(id){
 
@@ -45,4 +52,49 @@ function toggleStyle(id){
         rejectedBtn.classList.remove('bg-gray-300','text-black');
         rejectedBtn.classList.add('bg-blue-500','text-white');
     }
+    
+    const selected = document.getElementById(id)
+    console.log(selected);
+}
+
+// toggle btn inside card//
+mainContainer.addEventListener('click',function(event){
+    // console.log(event.target.parentNode.parentNode.parentNode);
+    const parentNode = event.target.parentNode.parentNode.parentNode;
+    const companyName = parentNode.querySelector('.companyName').innerText
+    const Position = parentNode.querySelector('.Position').innerText
+    const Type = parentNode.querySelector('.Type').innerText
+    const progress = parentNode.querySelector('.progress').innerText
+    const notes = parentNode.querySelector('.notes').innerText
+    
+
+    const cardInfo ={
+        companyName,
+        Position,
+        Type,
+        progress,
+        notes
+    }
+    // console.log(cardInfo);
+    
+  const companyExist = InterviewList.find(item=> item.companyName == cardInfo.companyName)
+  if(!companyExist){
+    InterviewList.push(cardInfo)
+  }
+
+ renderInterview()
+})
+
+function renderInterview(){
+  filterSection.innerHTML = ''
+
+  for(let Interview of InterviewList){
+    console.log(Interview);
+    let div = document.createElement('div');
+    div.className = 'flex justify-between bg-white p-4'
+    div.innerHTML =`
+    
+
+    `
+  }
 }
