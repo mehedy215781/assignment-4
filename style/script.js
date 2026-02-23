c5e0a7f0a778d8db2f9ad2b347c88e58c60ce408
mehedy215781 <mehedy215781@gmail.com>
2026-02-24 00:49:38 +0600

    render done both button

--- style/script.js
+++ style/script.js
@@ -1,5 +1,6 @@
 let interviewList = [];
 let rejectedList = [];
+let currentStatus = 'all'
 
 let total = document.getElementById('Total');
 let interviewCount = document.getElementById('Interview-count');
@@ -27,6 +28,7 @@ const rejectedBtn = document.getElementById('btn-rejected');
 
 function toggleStyle(id) {
 
+    
     // btn-click//
     allBtn.classList.remove('bg-blue-500', 'text-white');
     allBtn.classList.add('bg-gray-300', 'text-black');
@@ -49,14 +51,23 @@ function toggleStyle(id) {
         rejectedBtn.classList.add('bg-blue-500', 'text-white');
     }
 
-    // const selected = document.getElementById(id)
+       
+    const selected = document.getElementById(id)
+
+     currentStatus = id
+
     if(id == 'btn-interview'){
         allCardSections.classList.add('hidden');
         filterSection.classList.remove('hidden')
+        renderInterview()
     }else if(id == 'btn-all'){
 
      allCardSections.classList.remove('hidden');
     filterSection.classList.add('hidden')
+    } else if(id == 'btn-rejected'){
+        allCardSections.classList.add('hidden');
+        filterSection.classList.remove('hidden')
+        renderRejected()
     }
     
 }
@@ -100,9 +111,18 @@ console.log(event.target.classList.contains('interview'));
             interviewList.push(cardInfo);
 
         }
+
+        rejectedList =rejectedList.filter(item=> item.companyName != cardInfo.companyName)
+        
+      if(currentStatus == 'btn-interview'){
+            renderInterview();
+        }
+
+
         calculateTotall()
-       renderInterview();
-    }else if (event.target.classList.contains('btn-rejected')) {
+    //    renderInterview();
+
+    }else if (event.target.classList.contains('rejected')) {
         const parentNode = event.target.parentNode.parentNode.parentNode;
 
         const companyName = parentNode.querySelector('.companyName').innerText
@@ -129,14 +149,21 @@ console.log(event.target.classList.contains('interview'));
             rejectedList.push(cardInfo);
 
         }
+
+        interviewList = interviewList.filter(item=> item.companyName != cardInfo.companyName)
+
+        if(currentStatus == 'btn-interview'){
+            renderInterview();
+        }
+
         calculateTotall()
-       renderInterview();
+        // renderRejected()
     }
      
 })
   
 const filterSection = document.getElementById('filter-section');
-
+// interview-render/
 function renderInterview() {
 
     filterSection.innerHTML = '';
@@ -166,44 +193,41 @@ function renderInterview() {
           <a class="border-1 border-gray-300 p-2 rounded-[50%]" href=""><i class="fa-solid fa-trash-can"></i></a>
           
         </div>
-
+        
         `
-        filterSection.appendChild(div);
+         
     }
 }
+// rejected-render//
 
 function renderRejected() {
-
     filterSection.innerHTML = '';
 
     for (let reject of rejectedList) {
-        console.log(interview);
+        console.log(reject); 
 
         let div = document.createElement('div');
         div.className = 'flex justify-between bg-white p-4';
         div.innerHTML = `
-            <div class="left space-y-4 ">
-          <div>
-            <h4 class="companyName text-2xl font-bold">${interview.companyName}</h4>
-          <p class="Position text-[#64748B]">${interview.Position}</p>
-          </div>
-          <p class="Type  text-[#64748B]">Remote• Full-time • $130,000 - $175,000</p>
-            <button class="progress bg-[#dee5f5] p-2 rounded-sm">${interview.progress}</button>
-            <p class="notes text-[323B49]">${interview.notes}</p>
-          
-           <div>
-          <button  class=" interview bg-white cursor-pointer border  border-[#10B981] p-2 rounded-sm">Interview</button>
-           
-       <button  class=" rejected bg-white border cursor-pointer border-[#10B981] p-2 rounded-sm">Rejected</button>
-        </div>
-      </div>
-        <div class="right ">
-          <a class="border-1 border-gray-300 p-2 rounded-[50%]" href=""><i class="fa-solid fa-trash-can"></i></a>
-          
-        </div>
-
-        `
+            <div class="left space-y-4">
+                <div>
+                    <h4 class="companyName text-2xl font-bold">${reject.companyName}</h4>
+                    <p class="Position text-[#64748B]">${reject.Position}</p>
+                </div>
+                <p class="Type text-[#64748B]">Remote• Full-time • $130,000 - $175,000</p>
+                <button class="progress bg-[#dee5f5] p-2 rounded-sm">${reject.progress}</button>
+                <p class="notes text-[323B49]">${reject.notes}</p>
+                <div>
+                    <button class="interview bg-white cursor-pointer border border-[#10B981] p-2 rounded-sm">Interview</button>
+                    <button class="rejected bg-white border cursor-pointer border-[#10B981] p-2 rounded-sm">Rejected</button>
+                </div>
+            </div>
+            <div class="right">
+                <a class="border-1 border-gray-300 p-2 rounded-[50%]" href="">
+                    <i class="fa-solid fa-trash-can"></i>
+                </a>
+            </div>
+        `;
         filterSection.appendChild(div);
     }
 }
-
