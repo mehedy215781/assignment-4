47a7581159e41450fc7ee8c6fe0551af7e37f701
mehedy215781 <mehedy215781@gmail.com>
2026-02-23 16:17:06 +0600

    button target set inside every card

--- style/script.js
+++ style/script.js
@@ -1,27 +1,23 @@
- let InterviewList = [];
- let rejectedList  =[];
-
+let interviewList = [];
+let rejectedList = [];
 
 let total = document.getElementById('Total');
 let interviewCount = document.getElementById('Interview-count');
 let rejectedCount = document.getElementById('Rejected-count');
-console.log(interviewCount);
+// console.log(interviewCount);
 
 const allCardSections = document.getElementById('all-cards ');
-console.log(allCardSections.children.length);
+// console.log(allCardSections.children.length);
 
+
+// InterviewList.push({name:'compnay'},{name:'compnay'}) test
 
 const mainContainer = document.querySelector('main');
 const filterSection = document.getElementById('filter-section');
 
 // console.log(mainContainer);
 
-function calculateTotall (){
-    total.innerText = allCardSections.children.length;
-    interviewCount.innerText = InterviewList.length;
-     rejectedCount.innerText = rejectedList.length;
-}
-calculateTotall()
+
 
 // buttonColrchange//
 const allBtn = document.getElementById('btn-all');
@@ -29,31 +25,103 @@ const interviewBtn = document.getElementById('btn-Interview');
 const rejectedBtn = document.getElementById('btn-Rejected');
 
 
-function toggleStyle(id){
+function toggleStyle(id) {
 
     // btn-click//
-    allBtn.classList.remove('bg-blue-500','text-white');
-    allBtn.classList.add('bg-gray-300','text-black');
+    allBtn.classList.remove('bg-blue-500', 'text-white');
+    allBtn.classList.add('bg-gray-300', 'text-black');
 
-    interviewBtn.classList.remove('bg-white','text-white');
-    interviewBtn.classList.add('bg-gray-300','text-black');
+    interviewBtn.classList.remove('bg-white', 'text-white');
+    interviewBtn.classList.add('bg-gray-300', 'text-black');
 
-    rejectedBtn.classList.remove('bg-white','text-white');
-    rejectedBtn.classList.add('bg-gray-300','text-black');
+    rejectedBtn.classList.remove('bg-white', 'text-white');
+    rejectedBtn.classList.add('bg-gray-300', 'text-black');
 
     // click btn avtive//
-    if(id === 'btn-all'){
-        allBtn.classList.remove('bg-gray-300','text-black');
-        allBtn.classList.add('bg-blue-500','text-white');
-    } else if(id === 'btn-Interview'){
-        interviewBtn.classList.remove('bg-gray-300','text-black');
-        interviewBtn.classList.add('bg-blue-500','text-white');
-    } else if (id === 'btn-Rejected'){
-        rejectedBtn.classList.remove('bg-gray-300','text-black');
-        rejectedBtn.classList.add('bg-blue-500','text-white');
+    if (id === 'btn-all') {
+        allBtn.classList.remove('bg-gray-300', 'text-black');
+        allBtn.classList.add('bg-blue-500', 'text-white');
+    } else if (id === 'btn-Interview') {
+        interviewBtn.classList.remove('bg-gray-300', 'text-black');
+        interviewBtn.classList.add('bg-blue-500', 'text-white');
+    } else if (id === 'btn-Rejected') {
+        rejectedBtn.classList.remove('bg-gray-300', 'text-black');
+        rejectedBtn.classList.add('bg-blue-500', 'text-white');
     }
-    
+
     const selected = document.getElementById(id)
     // console.log(selected);
+}
+
+// calculate ///
+
+function calculateTotall() {
+    total.innerText = allCardSections.children.length;
+    interviewCount.innerText = interviewList.length;
+    rejectedCount.innerText = rejectedList.length;
+}
+calculateTotall()
+
+mainContainer.addEventListener('click', function (event) {
+console.log(event.target.classList.contains('interview'));
+
+    if (event.target.classList.contains('interview')) {
+        const parentNode = event.target.parentNode.parentNode.parentNode;
+
+        const companyName = parentNode.querySelector('.companyName').innerText
+        const Position = parentNode.querySelector('.Position').innerText
+        const Type = parentNode.querySelector('.Type').innerText
+        const progress = parentNode.querySelector('.progress').innerText
+        const notes = parentNode.querySelector('.notes').innerText
+
+        const cardInfo = {
+            companyName,
+            Position,
+            Type,
+            progress,
+            notes
+        }
+
+        const companyExist = interviewList.find(item => item.companyName === cardInfo.companyName)
+
+        if (!companyExist) {
+            interviewList.push(cardInfo);
+
+        }
+        renderInterview()
+    }
+})
+
+function renderInterview() {
+    filterSection.innerHTML = ''
+
+    for (let interview of interviewList) {
+        console.log(interview);
+
+        let div = document.createElement('div');
+        div.className = 'flex justify-between bg-white p-4'
+        div.innerHTML = `
+            <div class="left space-y-4 ">
+          <div>
+            <h4 class="companyName text-2xl font-bold">Mobile First Corp</h4>
+          <p class="Position text-[#64748B]">React Native Developer</p>
+          </div>
+          <p class="Type  text-[#64748B]">Remote• Full-time • $130,000 - $175,000</p>
+            <button class="progress bg-[#dee5f5] p-2 rounded-sm">Not Applied</button>
+            <p class="notes text-[323B49]">Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
+          
+          <div>
+          <button class=" interview bg-white cursor-pointer border border-[#10B981] p-2 rounded-sm"><span class="text-[#10B981] font-bold">Interview</button>
+           
+        <button class=" rejected bg-white border cursor-pointer border-[#10B981] p-2 rounded-sm"><span class="text-[#de3a3a] font-bold">Rejected</button>
+        </div>
+      </div>
+        <div class="right ">
+          <a class="border-1 border-gray-300 p-2 rounded-[50%]" href=""><i class="fa-solid fa-trash-can"></i></a>
+          
+        </div>
+
+        `
+    }
 }
 
