1266419ba22b56b6d35ef9a86444fdac508bf6f5
mehedy215781 <mehedy215781@gmail.com>
2026-02-23 21:08:22 +0600

    render-interview and rejected button and edited some logic

--- style/script.js
+++ style/script.js
@@ -13,7 +13,7 @@ const allCardSections = document.getElementById('all-cards ');
 // InterviewList.push({name:'compnay'},{name:'compnay'}) test
 
 const mainContainer = document.querySelector('main');
-const filterSection = document.getElementById('filter-section');
+
 
 // console.log(mainContainer);
 
@@ -21,8 +21,8 @@ const filterSection = document.getElementById('filter-section');
 
 // buttonColrchange//
 const allBtn = document.getElementById('btn-all');
-const interviewBtn = document.getElementById('btn-Interview');
-const rejectedBtn = document.getElementById('btn-Rejected');
+const interviewBtn = document.getElementById('btn-interview');
+const rejectedBtn = document.getElementById('btn-rejected');
 
 
 function toggleStyle(id) {
@@ -41,16 +41,24 @@ function toggleStyle(id) {
     if (id === 'btn-all') {
         allBtn.classList.remove('bg-gray-300', 'text-black');
         allBtn.classList.add('bg-blue-500', 'text-white');
-    } else if (id === 'btn-Interview') {
+    } else if (id === 'btn-interview') {
         interviewBtn.classList.remove('bg-gray-300', 'text-black');
         interviewBtn.classList.add('bg-blue-500', 'text-white');
-    } else if (id === 'btn-Rejected') {
+    } else if (id === 'btn-rejected') {
         rejectedBtn.classList.remove('bg-gray-300', 'text-black');
         rejectedBtn.classList.add('bg-blue-500', 'text-white');
     }
 
-    const selected = document.getElementById(id)
-    // console.log(selected);
+    // const selected = document.getElementById(id)
+    if(id == 'btn-interview'){
+        allCardSections.classList.add('hidden');
+        filterSection.classList.remove('hidden')
+    }else if(id == 'btn-all'){
+
+     allCardSections.classList.remove('hidden');
+    filterSection.classList.add('hidden')
+    }
+    
 }
 
 // calculate ///
@@ -74,46 +82,84 @@ console.log(event.target.classList.contains('interview'));
         const progress = parentNode.querySelector('.progress').innerText
         const notes = parentNode.querySelector('.notes').innerText
 
+        parentNode.querySelector('.progress').innerText = 'Applied'
+
         const cardInfo = {
             companyName,
             Position,
             Type,
-            progress,
+            progress:'Applied',
             notes
         }
 
         const companyExist = interviewList.find(item => item.companyName === cardInfo.companyName)
 
+         
+
         if (!companyExist) {
             interviewList.push(cardInfo);
 
         }
-        renderInterview()
+        calculateTotall()
+       renderInterview();
+    }else if (event.target.classList.contains('btn-rejected')) {
+        const parentNode = event.target.parentNode.parentNode.parentNode;
+
+        const companyName = parentNode.querySelector('.companyName').innerText
+        const Position = parentNode.querySelector('.Position').innerText
+        const Type = parentNode.querySelector('.Type').innerText
+        const progress = parentNode.querySelector('.progress').innerText
+        const notes = parentNode.querySelector('.notes').innerText
+
+        parentNode.querySelector('.progress').innerText = 'Rejected'
+
+        const cardInfo = {
+            companyName,
+            Position,
+            Type,
+            progress:'Rejected',
+            notes
+        }
+
+        const companyExist = rejectedList.find(item => item.companyName === cardInfo.companyName)
+
+         
+
+        if (!companyExist) {
+            rejectedList.push(cardInfo);
+
+        }
+        calculateTotall()
+       renderInterview();
     }
+     
 })
+  
+const filterSection = document.getElementById('filter-section');
 
 function renderInterview() {
-    filterSection.innerHTML = ''
+
+    filterSection.innerHTML = '';
 
     for (let interview of interviewList) {
         console.log(interview);
 
         let div = document.createElement('div');
-        div.className = 'flex justify-between bg-white p-4'
+        div.className = 'flex justify-between bg-white p-4';
         div.innerHTML = `
             <div class="left space-y-4 ">
           <div>
-            <h4 class="companyName text-2xl font-bold">Mobile First Corp</h4>
-          <p class="Position text-[#64748B]">React Native Developer</p>
+            <h4 class="companyName text-2xl font-bold">${interview.companyName}</h4>
+          <p class="Position text-[#64748B]">${interview.Position}</p>
           </div>
           <p class="Type  text-[#64748B]">Remote• Full-time • $130,000 - $175,000</p>
-            <button class="progress bg-[#dee5f5] p-2 rounded-sm">Not Applied</button>
-            <p class="notes text-[323B49]">Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
+            <button class="progress bg-[#dee5f5] p-2 rounded-sm">${interview.progress}</button>
+            <p class="notes text-[323B49]">${interview.notes}</p>
           
-          <div>
-          <button class=" interview bg-white cursor-pointer border border-[#10B981] p-2 rounded-sm"><span class="text-[#10B981] font-bold">Interview</button>
+           <div>
+          <button  class=" interview bg-white cursor-pointer border  border-[#10B981] p-2 rounded-sm">Interview</button>
            
-        <button class=" rejected bg-white border cursor-pointer border-[#10B981] p-2 rounded-sm"><span class="text-[#de3a3a] font-bold">Rejected</button>
+       <button  class=" rejected bg-white border cursor-pointer border-[#10B981] p-2 rounded-sm">Rejected</button>
         </div>
       </div>
         <div class="right ">
@@ -122,6 +168,42 @@ function renderInterview() {
         </div>
 
         `
+        filterSection.appendChild(div);
+    }
+}
+
+function renderRejected() {
+
+    filterSection.innerHTML = '';
+
+    for (let reject of rejectedList) {
+        console.log(interview);
+
+        let div = document.createElement('div');
+        div.className = 'flex justify-between bg-white p-4';
+        div.innerHTML = `
+            <div class="left space-y-4 ">
+          <div>
+            <h4 class="companyName text-2xl font-bold">${interview.companyName}</h4>
+          <p class="Position text-[#64748B]">${interview.Position}</p>
+          </div>
+          <p class="Type  text-[#64748B]">Remote• Full-time • $130,000 - $175,000</p>
+            <button class="progress bg-[#dee5f5] p-2 rounded-sm">${interview.progress}</button>
+            <p class="notes text-[323B49]">${interview.notes}</p>
+          
+           <div>
+          <button  class=" interview bg-white cursor-pointer border  border-[#10B981] p-2 rounded-sm">Interview</button>
+           
+       <button  class=" rejected bg-white border cursor-pointer border-[#10B981] p-2 rounded-sm">Rejected</button>
+        </div>
+      </div>
+        <div class="right ">
+          <a class="border-1 border-gray-300 p-2 rounded-[50%]" href=""><i class="fa-solid fa-trash-can"></i></a>
+          
+        </div>
+
+        `
+        filterSection.appendChild(div);
     }
 }
 
